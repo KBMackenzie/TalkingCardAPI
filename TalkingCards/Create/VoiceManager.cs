d8aeb1d5cd9b1929da3c2aadac90362ddb370ca8
KBMackenzie <109933089+KBMackenzie@users.noreply.github.com>
2022-12-24 15:56:44 -0300

    Added VoiceManager

---
+++ TalkingCards/Create/VoiceManager.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using InscryptionAPI.Sound;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using TalkingCardAPI.TalkingCards.Helpers;
+using UnityEngine;
+
+namespace TalkingCardAPI.TalkingCards.Create;
+
+[HarmonyPatch]
+internal static class VoiceManager
+{
+    private static readonly List<AudioClip> Voices = new();
+
+    public static void Add(string soundPath, string? soundId)
+    {
+        AudioClip voice = SoundManager.LoadAudioClip(soundPath);
+        voice.name = soundId ?? soundPath;
+        Voices.Add(voice);
+    }
+
+    [HarmonyPatch(typeof(AudioController), nameof(AudioController.GetAudioClip))]
+    [HarmonyPrefix]
+    private static void AddVoiceIds(ref List<AudioClip> ___SFX)
+    {
+        foreach(AudioClip voice in Voices)
+        {
+            if (!___SFX.Contains(voice))
+            {
+                ___SFX.Add(voice);
+            }
+        }
+    }
+}
