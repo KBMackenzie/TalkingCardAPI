22115f92f751c5214ff1e789cf40c878f6f3a172
KBMackenzie <109933089+KBMackenzie@users.noreply.github.com>
2022-12-26 15:41:42 -0300

    Changed version num

--- Plugin.cs
+++ Plugin.cs
@@ -12,7 +12,7 @@ public class Plugin : BaseUnityPlugin
 {   
     public const string PluginGuid = "kel.inscryption.talkingcardapi";
     public const string PluginName = "TalkingCardAPI";
-    public const string PluginVersion = "0.1.0";
+    public const string PluginVersion = "0.1.2";
 
     internal static Plugin? Instance;
 
